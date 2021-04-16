79213310d13695ac44ce77504e049dab5bbcfc02
Shruthi <shruthimarri97@gmail.com>
2021-04-16 13:34:05 +0530

    [Shruthi] : Added Welcome message o address book

---
+++ AddressBook/Address.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class Address
+    {
+        public static void AddressBook()
+        {
+            Console.WriteLine("Welcome to Address Book");
+        }
+    }
+}
