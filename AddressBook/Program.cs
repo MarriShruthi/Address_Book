4d30f75e909c569d61f99f522a8dffa7f05b8d50
Shruthi <shruthimarri97@gmail.com>
2021-04-22 13:03:10 +0530

    [Shruthi] : Added a contact in Address Book

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -6,7 +6,17 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            Address.AddressBook(); 
+            Console.WriteLine("--Welcome to Address Book ---");
+            Address person = new Address("Shruthi", "Marri", "Hyderabad", "Hyderabad", "Telangana", "501505", "9123456789", "shruthimarri97@gmail.com");
+            Console.WriteLine(person.getName());
+            Console.WriteLine(person.getlastName());
+            Console.WriteLine(person.getaddress());
+            Console.WriteLine(person.getcity());
+            Console.WriteLine(person.getstate());
+            Console.WriteLine(person.getzip());
+            Console.WriteLine(person.getphone());
+            Console.WriteLine(person.getemail());
+            Console.Read();
         }
     }
 }
