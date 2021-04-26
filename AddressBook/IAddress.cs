f41c7b540ef648fd5a95c8fd8501317348d75d01
Shruthi <shruthimarri97@gmail.com>
2021-04-26 20:03:35 +0530

    [Shruthi] : Added Delete contact

---
+++ AddressBook/IAddress.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    interface IAddress
+    {
+        void AddContact(string firstName, string lastName, string city, string state, string email, int zip, long phoneNumber);
+        void EditContact(string name);
+        void DeleteContact(string name);
+    }
+}
